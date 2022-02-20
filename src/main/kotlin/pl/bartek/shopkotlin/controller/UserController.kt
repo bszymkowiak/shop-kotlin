d3c33382b63d600ccfe7fc4599291fbb4ff07199
bszymkowiak <szymkowi.bartek@gmail.com>
2022-02-20 20:33:12 +0100

    add User

--- src/main/kotlin/pl/bartek/shopkotlin/controller/UserController.kt
+++ src/main/kotlin/pl/bartek/shopkotlin/controller/UserController.kt
@@ -1,27 +1,32 @@
 package pl.bartek.shopkotlin.controller
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.*
+import pl.bartek.shopkotlin.mapper.UserMapper
 import pl.bartek.shopkotlin.model.dao.User
+import pl.bartek.shopkotlin.model.dto.UserDto
 import pl.bartek.shopkotlin.service.UserService
 
 @RestController
 @RequestMapping("api/users")
-class UserController(val userService: UserService) {
+class UserController(val userService: UserService, val userMapper: UserMapper) {
 
     @PostMapping
-    fun saveUser(@RequestBody user: User): User {
-        return userService.save(user)
+    fun saveUser(@RequestBody userDto: UserDto): UserDto {
+        return userMapper.mapDaoToDto(userService.save(userMapper.mapDtoToDao(userDto)))
     }
 
     @GetMapping("/{id}")
-    fun getUserById(@PathVariable id: Long): User {
-        return userService.findById(id)
+    fun getUserById(@PathVariable id: Long): UserDto {
+        return userMapper.mapDaoToDto(userService.findById(id))
     }
 
-//    @GetMapping
-//    fun getUsers(@RequestParam page: Int, @RequestParam size: Int): Page<User> {
-//        return userService.getPage(PageRequest.of(page, size))
-//    }
+    @GetMapping
+    fun getUsers(@RequestParam page: Int, @RequestParam size: Int): Page<UserDto> {
+        return userService.getPage(PageRequest.of(page, size))
+            .map(userMapper::mapDaoToDto)
+    }
 
     @DeleteMapping("/{id}")
     fun deleteUserById(@PathVariable id: Long) {
@@ -29,8 +34,8 @@ class UserController(val userService: UserService) {
     }
 
     @PutMapping("/{id}")
-    fun updateUser(@RequestBody user: User, @PathVariable id: Long): User {
-        return userService.updateUser(id, user)
+    fun updateUser(@RequestBody userDto: UserDto, @PathVariable id: Long): UserDto {
+        return userMapper.mapDaoToDto(userService.updateUser(id, userMapper.mapDtoToDao(userDto)))
     }
 
 }
--- src/main/kotlin/pl/bartek/shopkotlin/model/dao/User.kt
+++ src/main/kotlin/pl/bartek/shopkotlin/model/dao/User.kt
@@ -1,11 +1,13 @@
 package pl.bartek.shopkotlin.model.dao
 
+import lombok.Builder
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
+@Builder
 data class User(
 
     @Id
